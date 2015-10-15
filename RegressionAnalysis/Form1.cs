0b0c7f57f510cbba2ebf2670a45140bb30442a7c
Markov Alex <markovalex95@gmail.com>
2015-10-16 01:36:49 +0300

    Сделал вычисление матрицы для СЛАУ

--- RegressionAnalysis/Form1.cs
+++ RegressionAnalysis/Form1.cs
@@ -36,14 +36,17 @@ namespace RegressionAnalysis
 				return;
 
 			var data = readData(dlg.FileName, new Func<double, double>[] { x => x, x2Lin, yLin });
-			
+			var a = calcSLAUMatrix(data);
 			
 		}
+
+
+		#region PARSING
 
 		//Читает данные из файла
 		//Осуществляет необходимые замены переменных,
 		//приводит данные к употребимому виду
-		double[][] readData(string filename, Func<double, double>[] convertors)
+		List<double[]> readData(string filename, Func<double, double>[] convertors)
 		{
 			//Создаем список, куда будем складывать значения
 			var list = new List<double[]>();
@@ -62,7 +65,7 @@ namespace RegressionAnalysis
 					list.Add(parseString(line, convertors));
 			}
 
-			return list.ToArray();
+			return list;
 		}
 
 		//Парсинг строки в массив вместе с преобразованием
@@ -70,27 +73,109 @@ namespace RegressionAnalysis
 		{
 			var strs = str.Split(';');
 			if (strs.Length != convertors.Length) throw new WrongNumberOfVariablesException();
-			double[] values = new double[strs.Length];
+			double[] values = new double[strs.Length+1];
+			values[0] = 1;	//Граязнохак для дальнейших вычислений
 
 			for(int i= 0; i<strs.Length; i++)
-				values[i] = convertors[i](Double.Parse(strs[i]));
+				values[i+1] = convertors[i](Double.Parse(strs[i]));
 
 			return values;
 		}
+
+		#endregion
 
 		//Функции для линеаризации значений
 		double x2Lin(double x2)
 		{
 			if (x2 <= 0)
 				throw new LogException(x2);
-			return  Math.Log(x2);
+			return x2; // Math.Log(x2);
 		}
 
 		double yLin(double y)
 		{
 			if (y <= 0)
 				throw new LogException(y);
-			return  Math.Log(y);
+			return y;//  Math.Log(y);
+		}
+
+		//Вычисление матрицы коэффициентов и свободных членов для СЛАУ
+		double[][] calcSLAUMatrix(List<double[]> rawData)
+		{
+
+			/*	Расчет коэффициентов:
+				N - число испытаний
+				М - число параметров
+	
+				Матрица для M=3
+
+				| N			 sum (Xi1)		sum (Xi2)     |
+				| sum (xi1)	 sum (xi1*xi1)	sum (xi2*xi1) |
+				| sum (xi2)	 sum (xi1*xi2)	sum (xi2*xi2) |
+
+				Можно видеть, что в первой строке и в первом
+				столбце отсутсвует домножение на Xik. Чтобы 
+				не писать для этих строк отдельный код, введем
+				ФИКТИВНЫЙ х в массив исходных данных, равный 1.
+
+				Было:     Стало:
+				X11 X12   1 X11 X12
+				X21 X22   1 X21 X22
+				...       ...    
+				Xn1 Xn2   1 Xn1 Xn2
+	          
+
+				Перепишем матрицу:
+        
+					1               2               3
+				1 | sum (xi0*xi0) 	sum (Xi1*xi0)	sum (Xi2*xi0) |
+				2 | sum (xi0*xi1)	sum (xi1*xi1)	sum (xi2*xi1) |
+				3 | sum (xi0*xi2)	sum (xi1*xi2)	sum (xi2*xi2) |
+
+				Аналогично для свободных членов
+
+				1 | sum (yi*xi0) |
+				2 | sum (yi*xi1) |
+				3 | sum (yi(xi2) |
+			*/
+
+			int N = rawData.Count;              //Число испытаний
+			int M = rawData[0].Length-1;        //Число аргументов = число переменных - y(1шт) + 1(a0) = число переменных
+
+			//Создаем матрицу коээфициентов
+			double[][] A = new double[M][];
+			for (int i = 0; i < M; i++)
+				A[i] = new double[M+1];
+
+			//Расчитываем
+			for (int k = 1; k <= M; k ++)
+			{
+				//k-номер строки
+				//Расчет K-й строки
+
+				//Расчет коэффициентов
+				//Из-за сдвига - нумерация строк с 1, а не 0
+				for (int i = 1; i <= M; i++)
+				{
+					//i - номер элемента в строке (номер столбца)
+					//Расчет элемента A[k,i]
+					A[k - 1][i - 1] = 0;
+					for (int j = 0; j < N; j++)
+					{
+						//j - номер эксперимента
+						A[k - 1][i - 1] += rawData[j][i - 1] * rawData[j][k - 1];
+					}
+				}
+
+				//Расчет свободных членов
+				A[k-1][M] = 0;
+				for(int j=0; j<N; j++)
+				{
+					A[k - 1][M] += rawData[j][M] * rawData[j][k-1];
+				}
+			}
+
+			return A;
 		}
 	}
 }
